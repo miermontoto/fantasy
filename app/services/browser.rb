35592ec68efddb47acc685f6f7799942df5ce395
Juan Mier <mier@mier.info>
2024-11-22 17:42:28 +0100

    cambiar entre ligas
    closes #16

--- app/services/browser.rb
+++ app/services/browser.rb
@@ -68,4 +68,27 @@ class Browser
   def communities
     @conn.post("/ajax/community-check")
   end
+
+  def change_community(id)
+    puts "cambiando a comunidad #{id}"
+
+    # si es un número, se cambia a esa comunidad
+    if id.is_a?(Integer) || id.to_i.to_s == id then
+      @conn.get("/action/change?id_community=#{id}")
+      return
+    end
+
+    # si es una cadena, hacer un LIKE y cambiar al primer resultado
+    list = Scraper.new.communities(self.communities.body)[:communities]
+    if list.nil? then; puts "error: no se ha podido obtener la lista de comunidades".red; return; end
+    list.each do |community|
+      if community.name.downcase.include?(id.downcase) then
+        puts "cambiando a comunidad #{community.name}"
+        self.change_community(community.id.to_i)
+        return
+      end
+    end
+
+    puts "error: no se ha encontrado ninguna comunidad con ese nombre".red
+  end
 end
--- main.rb
+++ main.rb
@@ -8,12 +8,19 @@ else
   browser = Browser.new
   scraper = Scraper.new
 
-  endpoints = [ 'feed', 'market', 'standings', 'team', 'offers', 'communities' ]
+  endpoints = [ 'feed', 'market', 'standings', 'team', 'offers' ]
   action = ARGV[0] || 'feed'
 
   if endpoints.include?(action)
     response = browser.send(action)
     scraper.send(action, response.body)
+  elsif action == 'communities'
+    if ARGV.length > 1 then
+      browser.change_community(ARGV[1])
+    else
+      response = browser.communities
+      scraper.communities(response.body)
+    end
   else
     puts "acción inválida [#{endpoints.join(', ')}]"
   end
